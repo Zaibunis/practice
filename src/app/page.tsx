f2e2d168d4bb1c8c3063cb368fddff92f89b3a6a
Zaibunis <zainabmustaqeem123@hotmail.com>
2024-12-15 00:24:42 +0500

    make more changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
-import Home from "./component/home"
+import Home from "./component/home";
+import Hero from "./component/hero";
+import Brands from "./component/brand";
+import NewArrivals from "./component/newArrivals";
+import TopSelling from "./component/topSelling";
+import DressStyle from "./component/DressStyle";
+import DressStyleCard from "./component/DressStyleCard";
 
 export default function Land() {
   return (
    <div>
   <Home />
+<Hero />
+<Brands />
+<NewArrivals />
+<TopSelling />
+<DressStyle />
+<DressStyleCard title={""} url={""} />
    </div>
   );
 }
